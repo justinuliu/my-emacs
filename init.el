58f06d9d28e11acc89bc8948d49761a89eb7c5f0
刘甲 <liujia01@chinayaan.com>
2014-08-31 15:01:20 +0800

    Use auto-complete-clang-extension

--- init.el
+++ init.el
@@ -14,30 +14,4 @@
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (el-get 'sync)
 
-
-(require 'auto-complete-config)
-(require 'auto-complete-clang)
-
-(setq clang-completion-suppress-error 't)
-
-(defun my-c-mode-common-hook()
-  (setq ac-auto-start nil)
-  (setq ac-expand-on-auto-complete nil)
-  (setq ac-quick-help-delay 0.3)
-  (define-key c-mode-base-map (kbd "M-/") 'auto-complete)
-)
-
-(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
-(setq ac-clang-flags  
-      (mapcar(lambda (item)(concat "-I" item))  
-	     (split-string  
-	      "
- /usr/include/c++/4.9
- /usr/include/x86_64-linux-gnu/c++/4.9
- /usr/include/c++/4.9/backward
- /usr/lib/gcc/x86_64-linux-gnu/4.9/include
- /usr/local/include
- /usr/lib/gcc/x86_64-linux-gnu/4.9/include-fixed
- /usr/include/x86_64-linux-gnu
- /usr/include
-")))
+(require 'auto-complete-clang-extension)
