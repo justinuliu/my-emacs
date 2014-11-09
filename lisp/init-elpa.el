4a99d4cc1eb50f8f2f193441f92e1db1c6272d76
刘甲 <liujia01@chinayaan.com>
2014-11-09 21:57:59 +0800

    Add marmalade repo.

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -26,6 +26,7 @@
 ;;; Also use Melpa for most packages
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 ;;; Also use marmalade
 ;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
