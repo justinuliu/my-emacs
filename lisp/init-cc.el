836492b832b7bb4a09e5a6f658264ea90c7ba454
刘甲 <liujia01@chinayaan.com>
2015-04-15 12:23:45 +0800

    Use helm for projectile completion.

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -153,6 +153,7 @@
 (require-package 'helm-projectile)
 (projectile-global-mode)
 (setq projectile-enable-caching t)
+(setq projectile-completion-system 'helm)
 
 ;;----------------------------------------------------------------------------
 ;; Init cpplint
