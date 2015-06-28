d6c3f070fe070fa5f734f761af8bc90c31882d4a
刘甲 <liujia01@chinayaan.com>
2015-06-28 14:51:01 +0800

    Adjust helm gtags key map.

--- lisp/init-helm-gtags.el
+++ lisp/init-helm-gtags.el
@@ -23,10 +23,17 @@
 (add-hook 'asm-mode-hook 'helm-gtags-mode)
 
 ;; key bindings
-(define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-select)
-(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+(eval-after-load "helm-gtags"
+  '(progn
+     ;; (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
+     ;; (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
+     ;; (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
+     (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+     (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-select)
+     (define-key helm-gtags-mode-map (kbd "M-g M-p") 'helm-gtags-parse-file)
+     (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+     (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+     (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)))
+
 
 (provide 'init-helm-gtags)
