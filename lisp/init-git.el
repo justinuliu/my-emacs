2d198d726d5de786adb56d857ac3c2ceca0bc4ee
刘甲 <liujia01@chinayaan.com>
2015-09-05 23:54:09 +0800

    Since magit upgrade, disabled some config.

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -1,7 +1,7 @@
 (require-package 'magit)
 (require-package 'git-blame)
-(require-package 'git-commit-mode)
-(require-package 'git-rebase-mode)
+;;(require-package 'git-commit-mode)
+;;(require-package 'git-rebase-mode)
 (require-package 'gitignore-mode)
 (require-package 'gitconfig-mode)
 (require-package 'git-messenger) ;; Though see also vc-annotate's "n" & "p" bindings
@@ -29,12 +29,12 @@
 
 ;;; When we start working on git-backed files, use git-wip if available
 
-(after-load 'magit
-  (global-magit-wip-save-mode)
-  (diminish 'magit-wip-save-mode))
+;; (after-load 'magit
+;;   (global-magit-wip-save-mode)
+;;   (diminish 'magit-wip-save-mode))
 
-(after-load 'magit
-  (diminish 'magit-auto-revert-mode))
+;; (after-load 'magit
+;;   (diminish 'magit-auto-revert-mode))
 
 
 (when *is-a-mac*
@@ -50,33 +50,33 @@
 
 ;;; git-svn support
 
-(require-package 'magit-svn)
-(autoload 'magit-svn-enabled "magit-svn")
-(defun sanityinc/maybe-enable-magit-svn-mode ()
-  (when (magit-svn-enabled)
-    (magit-svn-mode)))
-(add-hook 'magit-status-mode-hook #'sanityinc/maybe-enable-magit-svn-mode)
+;; (require-package 'magit-svn)
+;; (autoload 'magit-svn-enabled "magit-svn")
+;; (defun sanityinc/maybe-enable-magit-svn-mode ()
+;;   (when (magit-svn-enabled)
+;;     (magit-svn-mode)))
+;; (add-hook 'magit-status-mode-hook #'sanityinc/maybe-enable-magit-svn-mode)
 
-(after-load 'compile
-  (dolist (defn (list '(git-svn-updated "^\t[A-Z]\t\\(.*\\)$" 1 nil nil 0 1)
-                      '(git-svn-needs-update "^\\(.*\\): needs update$" 1 nil nil 2 1)))
-    (add-to-list 'compilation-error-regexp-alist-alist defn)
-    (add-to-list 'compilation-error-regexp-alist (car defn))))
+;; (after-load 'compile
+;;   (dolist (defn (list '(git-svn-updated "^\t[A-Z]\t\\(.*\\)$" 1 nil nil 0 1)
+;;                       '(git-svn-needs-update "^\\(.*\\): needs update$" 1 nil nil 2 1)))
+;;     (add-to-list 'compilation-error-regexp-alist-alist defn)
+;;     (add-to-list 'compilation-error-regexp-alist (car defn))))
 
-(defvar git-svn--available-commands nil "Cached list of git svn subcommands")
+;; (defvar git-svn--available-commands nil "Cached list of git svn subcommands")
 
-(defun git-svn (dir)
-  "Run a git svn subcommand in DIR."
-  (interactive "DSelect directory: ")
-  (unless git-svn--available-commands
-    (setq git-svn--available-commands
-          (sanityinc/string-all-matches
-           "^  \\([a-z\\-]+\\) +"
-           (shell-command-to-string "git svn help") 1)))
-  (let* ((default-directory (vc-git-root dir))
-         (compilation-buffer-name-function (lambda (major-mode-name) "*git-svn*")))
-    (compile (concat "git svn "
-                     (ido-completing-read "git-svn command: " git-svn--available-commands nil t)))))
+;; (defun git-svn (dir)
+;;   "Run a git svn subcommand in DIR."
+;;   (interactive "DSelect directory: ")
+;;   (unless git-svn--available-commands
+;;     (setq git-svn--available-commands
+;;           (sanityinc/string-all-matches
+;;            "^  \\([a-z\\-]+\\) +"
+;;            (shell-command-to-string "git svn help") 1)))
+;;   (let* ((default-directory (vc-git-root dir))
+;;          (compilation-buffer-name-function (lambda (major-mode-name) "*git-svn*")))
+;;     (compile (concat "git svn "
+;;                      (ido-completing-read "git-svn command: " git-svn--available-commands nil t)))))
 
 
 (require-package 'git-messenger)
