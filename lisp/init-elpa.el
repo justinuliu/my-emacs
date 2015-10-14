fb53904503b41163136b6f4b4ca38b80c7ff8c8b
刘甲 <liujia01@chinayaan.com>
2015-10-14 09:56:39 +0800

    Use melpa repository.

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -14,10 +14,8 @@
 
 ;;; Standard package repositories
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
 
 ;; If gpg cannot be found, signature checking will fail, so we
 ;; conditionally enable it according to whether gpg is available. We
